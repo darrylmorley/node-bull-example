7e0bb86c5ee589d3e086d300dd41d0bcfca4a8c8
Darryl Morley <darrylmorley.uk@gmail.com>
2024-02-28 11:55:04 +0000

    demo complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Use rate limiting to thorottle the number of jobs processed per second
+// Job failure handling
 
 import Bull from "bull";
 import dotenv from "dotenv";
@@ -26,19 +26,20 @@ burgerQueue.process(async (payload, done) => {
     // step 1
     payload.log("Step 1: Preparing the bun");
     payload.progress(25);
-    await sleep(5000);
+    await sleep(2000);
     // step 2
+    if (Math.random() > 0.25) throw new Error("Patty is burnt");
     payload.log("Step 2: Grilling the patty");
     payload.progress(50);
-    await sleep(5000);
+    await sleep(2000);
     // step 3
     payload.log("Step 3: Adding cheese");
     payload.progress(75);
-    await sleep(5000);
+    await sleep(2000);
     // step 4
     payload.log("Step 4: Adding toppings");
     payload.progress(100);
-    sleep(5000);
+    sleep(2000);
     // done
     payload.log("Done: Burger is ready");
     done();
@@ -54,4 +55,5 @@ const jobs = [...new Array(10)].map(_ => ({
   cheese: "cheddar",
   toppings: ["lettuce", "tomato", "onion"],
 }));
-jobs.forEach(job => burgerQueue.add(job));
+// specify retry options
+jobs.forEach(job => burgerQueue.add(job, {attempts: 3, backoff: 1000}));
